5e7add529df704effdc7f0b7953a1429eab3b65a
Gregg Wong <griekup@live.com>
2021-02-13 22:05:56 -0500

    Finished Lesson 7

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -9,9 +9,23 @@
 </template>
 
 <script>
+import { onBeforeUnmount } from 'vue';
     export default {
         name: 'ModalView',
         setup(props, {emit}) {
+            let onKeydown = (event) => {
+                console.log(event.key);
+                
+            if (event.key == 'Escape') {
+                emit('closeModal');
+            }
+            };
+
+
+            window.addEventListener('keydown', onKeydown);
+            onBeforeUnmount(() => {
+                window.removeEventListener('keydown', onKeydown);
+            })
             return {
                 emit
             }
