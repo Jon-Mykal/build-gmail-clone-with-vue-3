2fd359e4de01a9975a1f68008b033befa9723c61
Gregg Wong <griekup@live.com>
2021-02-14 15:25:49 -0500

    Finished Lesson 9

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="email-display">
+      <section>
+          <button @click="toggleArchive">{{ email.archived? 'Move to Inbox (e)' : 'Archive (e)'}}</button>
+          <button @click="toggleRead">{{ email.read ? 'Mark Unread (r)' : 'Mark Read (r)' }}</button>
+          <button @click="goNewer">Newer (k)</button>
+          <button @click="goOlder">Older (j)</button>
+      </section>
     <h2 class="mb-0">Subject: <strong>{{email.subject}}</strong></h2>
     <div><em>From {{email.from}} on {{ format(new Date(email.sentAt), 'MMM do yyyy')}}</em></div>
     <div v-html="marked(email.body)" />
@@ -9,12 +15,31 @@
 <script>
 import { format } from 'date-fns';
 import marked from 'marked';
+import axios from 'axios';
+import useKeydown from '../composables/use-keydown'
 
     export default {
-        setup () {
+        setup (props) {
+            let email = props.email;
+            // way to create methods with new api.
+            let toggleRead = () => {
+                email.read = !email.read;
+                axios.put(`http://localhost:3000/emails/${email.id}`, email);
+            };
+            let toggleArchive = () => {
+                email.archived = !email.archived;
+                axios.put(`http://localhost:3000/emails/${email.id}`, email);
+            };
+
+            useKeydown([
+                { key: 'r', fn: toggleRead },
+                { key: 'e', fn: toggleArchive }
+            ]);
             return {
                 format,
-                marked
+                marked,
+                toggleRead,
+                toggleArchive
             }
         },
         name: 'MailView',
